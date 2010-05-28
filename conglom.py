a5bfd0cdac600aa9b644e235a775d8338fd37cc2
Sean Sherrard <mssherrard@gmail.com>
2010-05-28 15:26:45 -0600

    first cut at conglomerate

---
+++ conglom.py
@@ -0,0 +1,67 @@
+import re
+
+def parse(courses):
+    ctree = {}
+    for crsln in courses:
+        match = re.match(r'([A-Z] [A-Z][A-Z]|[A-Z][A-Z] [A-Z]|[A-Z] [A-Z]|[A-Z]{2,4})\s*(\d{3,4}[A-Z]?)(?:\s*-\s*|\s+)(\d{1,2})(?:\s+|:\s*)(.*), (\w+) (\d+)', crsln, re.I)
+        if match:
+            dept = match.group(1).upper()
+            cnum = match.group(2).upper()
+            sect = match.group(3)
+            desc = match.group(4)
+            term = match.group(5)
+            year = match.group(6)
+            if len(cnum) == 3 or len(cnum) == 4 and not cnum.isdigit():
+                cnum = cnum[0] + cnum
+            if len(sect) == 1:
+                sect = '0' + sect
+            ctree.setdefault(dept, {}).setdefault(cnum, set()).add(sect)
+        else:
+            print "no match"
+    if len(ctree) > 1:
+        res = ' / '.join(dept + ' ' + cnum + ' - ' + ', '.join(sorted(ctree[dept][cnum]))
+                         for dept in sorted(ctree) for cnum in sorted(ctree[dept]))
+    else:
+        res = ' / '.join(dept + ' ' + ' / '.join(cnum + ' - ' + ', '.join(sorted(ctree[dept][cnum]))
+                         for cnum in sorted(ctree[dept])) for dept in sorted(ctree))
+    return res, ctree
+
+
+def elide(course):
+    sects = sorted(course)
+    idx = 0
+    beg = 0
+    while idx <= len(sects):
+        if idx < len(sects) and elen == int(sects[idx]) - int(sects[beg]):
+            elen += 1
+        else:
+            if idx - beg > 2:
+                sects[beg:idx] = [sects[beg] + ' ... ' + sects[idx-1]]
+                idx = beg + 1
+            beg = idx
+        idx += 1
+    return sects
+##    for idx in range(len(sections)):
+##        if int(sections[idx]) - int(sections[idx-1]) > 1:
+#    last = ?
+    for sect in sorted(course):
+        if int(sect) - int(last) > 1:
+            if int(last) - int(mark) > 1:
+                res += ' ... ' + last + ', '
+            else:
+                res += ', ' + last
+            res += sect
+            mark = sect
+        last = sect
+
+def foo(courses):
+    cd = {}
+    for course in courses:
+        cd.setdefault(course[0], {}).setdefault(course[1], {}).setdefault(course[2], course)
+    if len(cd) > 1:
+        res = ' / '.join(dept + ' ' + cnum + ' - ' + ', '.join(sorted(cd[dept][cnum]))
+                         for dept in sorted(cd) for cnum in sorted(cd[dept]))
+    else:
+        res = ' / '.join(dept + ' ' + ' / '.join(cnum + ' - ' + ', '.join(sorted(cd[dept][cnum]))
+                         for cnum in sorted(cd[dept])) for dept in sorted(cd))
+    return res
